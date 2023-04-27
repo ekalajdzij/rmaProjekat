68422a6050d84afb3d2f1b6cbee5eb28e6d84fea
Emir <ekalajdzij1@etf.unsa.ba>
2023-04-27 08:20:07 +0200

    Added a third test, fixed some issues in other tests

--- app/src/androidTest/java/ba/etf/unsa/rma/videogameproject/OwnEspressoTests.kt
+++ app/src/androidTest/java/ba/etf/unsa/rma/videogameproject/OwnEspressoTests.kt
@@ -1,101 +1,70 @@
 package ba.etf.unsa.rma.videogameproject
 
-import android.app.IntentService
+import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
-import android.util.Log
 import android.view.View
 import android.widget.TextView
-import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.NoMatchingViewException
-import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.PositionAssertions
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 
-import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 
 
-
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.*
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.platform.app.InstrumentationRegistry
-import junit.framework.TestCase.assertEquals
-import org.hamcrest.CoreMatchers
-import org.hamcrest.CoreMatchers.anything
+import org.hamcrest.*
 import org.hamcrest.CoreMatchers.not
-import org.hamcrest.Matchers
-import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 
 class OwnEspressoTests {
-    fun hasItemCount(n: Int) = ViewAssertion { view, noViewFoundException ->
-        if (noViewFoundException != null) {
-            throw noViewFoundException
-        }
-        Assert.assertTrue("View nije tipa RecyclerView", view is RecyclerView)
-        var rv: RecyclerView = view as RecyclerView
-        ViewMatchers.assertThat(
-            "GetItemCount RecyclerView broj elementa: ",
-            rv.adapter?.itemCount,
-            CoreMatchers.`is`(n)
-        )
-    }
-
 
     @get:Rule
     var homeRule: ActivityScenarioRule<HomeActivity> = ActivityScenarioRule(HomeActivity::class.java)
 
 
     /**
-     * test1 se odnosi na zahtjeve koje smo imali u zadatku
+     * prvi test se odnosi na zahtjeve koje smo imali u zadatku
      * pri prvom otvaranju aplikacije zahtjev je da navigation bude disabled, pa provjeravamo to
-     * zatim kada kliknemo na igricu i otvorimo game details provjerit cemo da li je detailsItem visible, trebalo bi da nije
      * zatim cemo se klikom na home vratiti na HomeFragment i provjeriti da li je homeItem enabled, trebalo bi da nije
      * nakon te provjere izvrsit cemo klik na gameDetailsItem i
      * provjeriti da li se u GameFragmentu otvorila ispravna igrica tako sto cemo provjeriti game-title
      */
     @Test
-    fun test1(){
+    fun requirementsTest(){
+
         val home = onView(withId(R.id.homeItem))
         home.check(matches(not(isEnabled())))
 
         val details = onView(withId(R.id.gameDetailsItem))
         details.check(matches(not(isEnabled())))
 
-        Thread.sleep(1000)
         onView(withId(R.id.game_list)).perform(click())
-
-        details.check(matches(not(isDisplayed())))
 
         onView(withId(R.id.homeItem)).perform(click())
-        Thread.sleep(1000)
         home.check(matches(not(isEnabled())))
 
-        val name = onView(withId(R.id.item_title_textview))
-        Thread.sleep(1000)
-        onView(withId(R.id.game_list)).perform(click())
-        name.check(matches(withId(R.id.item_title_textview)))
+        onView(withId(R.id.game_list)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0)).check(
+            matches(
+                hasDescendant(withText(getText(withIndex(withId(R.id.item_title_textview),0))))
+            ))
+        homeRule.scenario.close()
 
     }
 
     /**
-     * test2 se odnosi na testiranje rasporeda elemenata u GameDetailsFragmentu
+     * drugi test se odnosi na testiranje rasporeda elemenata u GameDetailsFragmentu
      * prvo se vrši klik na jednu igru sa HomeActivity-ja, zatim se vraća na home, pa se vrši klik na gameDetailsItem
      * nakon toga se vrši test pozicije elemenata u GameDetailsFragmentu
      */
     @Test
-    fun test2(){
+    fun detailsFragmentAppearance(){
         onView(withId(R.id.game_list)).perform(click())
-        Thread.sleep(1000)
         onView(withId(R.id.homeItem)).perform(click())
         onView(withId(R.id.gameDetailsItem)).perform(click())
         onView(withId(R.id.item_title_textview)).check(PositionAssertions.isCompletelyBelow(withId(R.id.logo_image)))
@@ -106,34 +75,72 @@ class OwnEspressoTests {
         onView(withId(R.id.developer_textview)).check(PositionAssertions.isRightAlignedWith(withId(R.id.publisher_textview)))
         onView(withId(R.id.publisher_textview)).check(PositionAssertions.isRightAlignedWith(withId(R.id.genre_textview)))
         onView(withId(R.id.genre_textview)).check(PositionAssertions.isRightAlignedWith(withId(R.id.description_textview)))
+
+        homeRule.scenario.close()
     }
 
+    fun withIndex(matcher: Matcher<View?>, index: Int): Matcher<View?> {
+        return object : TypeSafeMatcher<View>() {
+            var currentIndex = 0
+            var viewObjHash = 0
+
+            @SuppressLint("DefaultLocale")
+            override fun describeTo(description: Description) {
+                description.appendText(String.format("with index: %d ", index))
+                matcher.describeTo(description)
+            }
+
+            override fun matchesSafely(view: View): Boolean {
+                if (matcher.matches(view) && currentIndex++ == index) {
+                    viewObjHash = view.hashCode()
+                }
+                return view.hashCode() == viewObjHash
+            }
+        }
+    }
+
+
+    fun getText(matcher: Matcher<View?>?): String? {
+        val stringHolder = arrayOf<String?>(null)
+        onView(matcher).perform(object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isAssignableFrom(TextView::class.java)
+            }
+
+            override fun getDescription(): String {
+                return "getting text from a TextView"
+            }
+
+            override fun perform(uiController: UiController, view: View) {
+                val tv = view as TextView //Save, because of check in getConstraints()
+                stringHolder[0] = tv.text.toString()
+            }
+        })
+        return stringHolder[0]
+    }
     /**
-     * testiramo prikaz svih potrebnih informacija u GameDetailsActivity nakon nekoliko promjena izmedju HomeActivity i GameDetailActivity
-     * s ciljem provjere da li je posljednja igra kojoj se pristupa dobro zapamcena
-     * nakon posljednjeg pristupa GameDetailActivity provjeravamo da li su sve informacije u redu
-     *
+     * pri pokretanju aplikacije provjerit cemo da li je navigation disable
+     * zatim cemo izvrsiti klik na igru iz liste igara
+     * kada dobijemo detalje igre izvrsit cemo klik na home opciju navigationa
+     * kada se vratimo na home izvrsit cemo promjenu orijentacije u landscape mode
+     * u landscape-u testiramo da li je prikazan title za odgovarajucu igru
      */
 
     @Test
-    fun testTitleInFragments() {
-        val activityScenario = ActivityScenario.launch(HomeActivity::class.java)
-        activityScenario.onActivity { activity ->
-            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+    fun testTitleInLandscape() {
 
-            val fragmentManager = activity.supportFragmentManager
+        onView(withId(R.id.game_list)).perform(click())
+        onView(withId(R.id.homeItem)).perform(click())
 
-            val homeFragment = fragmentManager.findFragmentById(R.id.fragment_container_left) as HomeFragment
-            val gameDetailsFragment = fragmentManager.findFragmentById(R.id.fragment_container_right) as GameDetailsFragment
-
-            val gameList = homeFragment.view?.findViewById<RecyclerView>(R.id.game_list)
-            val expectedTitle = gameList?.findViewById<TextView>(R.id.item_title_textview)?.text.toString()
-            val actualTitle = gameDetailsFragment.view?.findViewById<TextView>(R.id.item_title_textview)?.text.toString()
-
-            assertEquals(expectedTitle, actualTitle)
-            Log.d("provjeeeeeeeeeeeeeeeeeeeeera", expectedTitle)
-            Log.d("provjeeeeeeeeeeeeeeeeeeeeera", actualTitle)
-
+        homeRule.scenario.onActivity {
+            it.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         }
+        onView(withId(R.id.game_list)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0)).check(
+            matches(
+                hasDescendant(withText(getText(withIndex(withId(R.id.item_title_textview),0))))
+            ))
+        homeRule.scenario.close()
+
     }
+
 }
